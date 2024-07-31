e224208f9dbe43d412eebd7abc290efbd1d9467f
Ritesh <ritesh.nayak@dxc.com>
2024-07-31 16:26:40 +0530

    Made changes in PLP test script

--- src/test/java/com/hpeb2c/testcases/TC_PLPTest_005.java
+++ src/test/java/com/hpeb2c/testcases/TC_PLPTest_005.java
@@ -37,16 +37,33 @@ public class TC_PLPTest_005 extends BaseClass {
 			// System.out.println(i);
 			System.out.println("product names are " + productname);
 
-			if (productname.contains("HPE ProLiant ML350 Gen11")) {
+			if (productname.contains("HPE ProLiant ML110 Gen11")) {
 				Thread.sleep(3000);
 				driver.findElement(By.xpath("//a[@class='enable-analytics brand-color']")).click();
 				break;
 			}
 		}
+		//h2[@class='hpe-headline hpe-headline--strong hpe-headline--small hpe-product-list__name']
 		
-		WebElement  requestquote = driver.findElement(By.xpath("//button[contains(text(),'Request Quote')]"));
-		JavascriptExecutor clickquotebutton = (JavascriptExecutor) driver;
-		clickquotebutton.executeScript("arguments[0].click();",requestquote);
+		
+		List<WebElement> btn = driver.findElements(By.xpath(
+				"//div[@class='hpe-product-list__meta row product-models-slot']"));
+		for (int i = 0; i < btn.size(); i++) {
+			String productname = btn.get(i).getText();
+			// System.out.println(i);
+			System.out.println("product names are " + productname);
+
+			if (productname.contains("Request Quote")) {
+				Thread.sleep(3000);
+				
+				WebElement  requestquote = driver.findElement(By.xpath("//button[contains(text(),'Request Quote')]"));
+				JavascriptExecutor clickquotebutton = (JavascriptExecutor) driver;
+				clickquotebutton.executeScript("arguments[0].click();",requestquote);
+				break;
+			}
+		}
+		
+		
 		
 	
 	}
